156d63f70af33cb52cd2c7c6db03396826c696ec
pranabk99 <166788541+pranabk99@users.noreply.github.com>
2026-03-12 14:21:02 +0530

    isTrustValidationRequired and isSignatureVerificationRequired set to false

--- authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/filter/IdAuthFilter.java
+++ authentication/authentication-common/src/main/java/io/mosip/authentication/common/service/filter/IdAuthFilter.java
@@ -1188,7 +1188,7 @@ public abstract class IdAuthFilter extends BaseAuthFilter {
 	 */
 	@Override
 	protected boolean isSignatureVerificationRequired() {
-		return true;
+		return false;
 	}
 
 	/**
@@ -1221,7 +1221,7 @@ public abstract class IdAuthFilter extends BaseAuthFilter {
 	 */
 	@Override
 	protected boolean isTrustValidationRequired() {
-		return true;
+		return false;
 	}
 	
 	/**
